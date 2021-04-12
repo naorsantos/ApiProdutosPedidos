ad812eeac35eb02e216ecf8b9032ca011d865a53
naorsantos <naorsantos@gmail.com>
2021-04-12 10:15:56 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/naorsantos/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.naorsantos.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.naorsantos.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
